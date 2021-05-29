ebc7e812b048b06ebd3e15193d4f60f10715f895
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-29 09:38:31 -0300

    feat: using cors-anywhere proxy to avoid CORS block

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 const APP_CONSTANTS = {
-  SHEETS_BASE_URL: 'https://product-sheets-api.herokuapp.com',
-  REGISTER_BASE_URL: 'https://product-register-api.herokuapp.com',
+  SHEETS_BASE_URL: 'https://cors-anywhere.herokuapp.com/product-sheets-api.herokuapp.com',
+  REGISTER_BASE_URL: 'https://cors-anywhere.herokuapp.com/product-register-api.herokuapp.com',
 };
 
 export { APP_CONSTANTS };
